d95592d32b5448d93801c81aa5c9239ab63ed1cb
arunrathi10 <200587971@student.georgianc.on.ca>
2025-04-07 16:22:06 -0400

    created a function createNewDeck

--- js/script.js
+++ js/script.js
@@ -3,3 +3,9 @@ const container = document.getElementById("card-container");
 const characterBox = document.getElementById("character-box");
 
 let winnerCard = null; // variable to randomnly choose winning card
+
+// created function for getting new set of cards
+function createNewDeck(){
+    characterBox.innerHTML = "";
+    container.innerHTML = "";
+}
