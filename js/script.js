8888fd6154c97cfeb0cf6ed55316ae8936d7fa35
arunrathi10 <200587971@student.georgianc.on.ca>
2025-04-07 16:18:06 -0400

    created a var

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
 // DOM elements
 const container = document.getElementById("card-container");
 const characterBox = document.getElementById("character-box");
+
+let winnerCard = null; // variable to randomnly choose winning card
